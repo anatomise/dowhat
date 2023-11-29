f78fb36a041042c6cf529c786bdd4c7f2f4d9fff
anatomise <73890878+anatomise@users.noreply.github.com>
2023-11-30 06:12:08 +0800

    v1.0

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 import express from "express";
 const app = express();
 const port = 3000;
+import axios from "axios";
 //set view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -22,13 +23,14 @@ mongoose.connect("mongodb+srv://xugeraldine:iCBoex8EIpw66XiT@techup.skdlpeo.mong
 
 //define database schema 
 const eventSchema = {
- name: String,
- location: String,
- start: Date,
- end: Date,
- tags: [String],
- comments: [{}]
-}
+    name: String,
+    location: String,
+    start: Date,
+    end: Date,
+    tags: [String],
+    comments: [{}],
+    googlePlaceId: String 
+  };
 
 const Event = mongoose.model("Event", eventSchema);
 
@@ -53,13 +55,14 @@ app.post("/submit", [
     check('name').trim().escape().notEmpty().withMessage('must not be blank'), 
     check('location').trim().escape().notEmpty().withMessage('must not be blank'),],
 (req, res) => {
-let { name, location, start, end, tags } = req.body;
+let { name, location, start, end, tags, googlePlaceId} = req.body;
 let newEvent = new Event({
     name: req.body.name,
     location: req.body.location,
     start: req.body.start,
     end: req.body.end,
     tags: req.body.tags,
+    googlePlaceId: req.body.googlePlaceId,    
 });
 
 //handle request only if no errors
@@ -67,18 +70,62 @@ const errors = validationResult(req);
 //console.log(errors);
 if (errors.isEmpty()) { //save, acknowledge submission, and show input
     newEvent.save(); 
-    return res.render("pages/thanks", { name, location, start, end, tags })};
+    return res.render("pages/thanks", { name, location, start, end, tags, googlePlaceId })};
 //otherwise, show error
 return res.render("pages/error");
 });
 
 //event listing page
-  app.get("/events", async (req, res) => {
-    const events = await Event.find({});
-    res.render('pages/events', {
-        eventsList: events
+//return events only after current date
+    const targetDate = new Date().toJSON();
+    console.log(targetDate);
+    app.get("/events", async (req, res) => {
+    const events = await Event.find({start: { $gt: targetDate }})
+                                    .sort({ start: 1 });
+//format date to be more human readable
+    const formattedEvents = events.map(event => ({
+        ...event._doc,
+        start: event.start.toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' }) + ' ' +
+               event.start.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+               end: event.end.toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' }) + ' ' +
+               event.end.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+               tags: event.tags.join(', ')
+    }));
+//render page
+       res.render('pages/events', {
+        eventsList: formattedEvents
     });
 });
+
+//get events from MongoDB
+app.get("/events-data", async (req, res) => {
+    try {
+        const dbevents = await Event.find();
+        res.json(dbevents);
+    } catch (error) {
+        console.error("Error fetching events:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+// Add a new endpoint for place details proxy
+app.get("/place-details", async (req, res) => {
+    const { place_id } = req.query;
+
+    try {
+        const placeDetailsResponse = await axios.get(
+            `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&key=AIzaSyAmxDo1bfDzW85IjuiwzbhDyr7Cs1ImeSA`
+        );
+
+     //   console.log("Place Details Response:", placeDetailsResponse.data);
+
+        res.json(placeDetailsResponse.data);
+    } catch (error) {
+        console.error("Error fetching place details:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`); //check that port is open
